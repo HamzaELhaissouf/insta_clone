efc5856f3d16816d00b4c5dfc76a24da4651a4ae
Hamza <Hamza>
2020-08-01 19:29:58 +0100

    finishin up

--- src/compontents/Header.js
+++ src/compontents/Header.js
@@ -4,7 +4,13 @@ import {stories} from '../Data';
 import Story from './Story';
 const Header = () => {
   return (
-    <ScrollView>
+    <ScrollView
+      horizontal={true}
+      contentContainerStyle={{width: `${100 * 10}%`}}
+      showsHorizontalScrollIndicator={false}
+      scrollEventThrottle={200}
+      decelerationRate="fast"
+      pagingEnabled>
       {stories.map((story) => (
         <Story story={story} key={story.id} />
       ))}
